378b4ff5b2d3ba39683250a38cfce7afa496cc88
salteadorneo <hola@salteadorneo.dev>
2022-07-05 19:53:20 +0200

    remove logo

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import Logo from '../components/Logo.svelte';
-
 	import '../app.css';
 	
 	import { onMount } from 'svelte';
@@ -14,6 +12,5 @@
 </script>
 
 <main class="max-w-6xl m-auto px-4 relative">
-	<Logo />
 	<slot />
 </main>
