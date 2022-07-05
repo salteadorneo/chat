5edd111bf48c7b17b0620a7e9e64fd5b8bcd9687
salteadorneo <hola@salteadorneo.dev>
2022-07-05 19:53:10 +0200

    logout

--- src/components/LoggedUserHeader.svelte
+++ src/components/LoggedUserHeader.svelte
@@ -1,13 +1,17 @@
 <script>
 	import { user } from '../store';
 	import LogoutIcon from '../components/LogoutIcon.svelte';
+
+	function handleLogout() {
+		user.set(null);
+		localStorage.removeItem('user');
+	}
 </script>
 
 {#if $user}
 	<strong class="text-zinc-700">{$user.name}</strong>
 	<img class="rounded-full h-8 w-8" alt={$user.name} src={$user.avatar} />
-	<button class="leading-none py-2 flex items-center gap-1 hover:underline">
-		<LogoutIcon />
+	<button on:click={handleLogout} class="leading-none py-2 flex items-center gap-1 hover:underline">
 		Cerrar sesión
 	</button>
 {/if}
