6de418d2eb08c49a7edc650b1ad9690c44fbc746
salteadorneo <hola@salteadorneo.dev>
2022-07-13 01:18:55 +0200

    remove vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,20 +10,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: vercel({
-			// if true, will deploy the app using edge functions
-			// (https://vercel.com/docs/concepts/functions/edge-functions)
-			// rather than serverless functions
-			edge: false,
-		
-			// an array of dependencies that esbuild should treat
-			// as external when bundling functions
-			external: [],
-		
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app
-			split: false
-		}),
+		adapter: adapter(),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
